b3e44bf2d5c4d950e2d86f57d6bf3b2182571e54
Rohit Jadhav <rohit.jadhav0403@gmail.com>
2024-10-26 14:15:16 +0530

    added the database and select query

---
+++ db/connectDatabase.go
@@ -0,0 +1,15 @@
+package db
+
+import (
+	"database/sql"
+	"errors"
+)
+
+func Connect() (*sql.DB, error) {
+	db, err := sql.Open("mysql", "testing_go:$7)BW,_;Ibk8@tcp(68.178.150.12:3306)/rngdev_testing_go")
+	if err != nil {
+		return nil, errors.New(err.Error())
+	}
+
+	return db, nil
+}
--- handler/loginController.go
+++ handler/loginController.go
@@ -2,10 +2,30 @@ package handler
 
 import (
 	"net/http"
+	"rngdev/db"
 
 	"github.com/gin-gonic/gin"
 )
 
+type Emp struct {
+	Username string `json:"name"`
+	Age      string `json:"age"`
+}
+
 func Login(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"message": "Hello Ram"})
+	db, err := db.Connect()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+	}
+
+	res, err := db.Query("select username , age from users")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+	}
+	var emps []Emp
+	for res.Next() {
+		var emp Emp
+		emps = append(emps, Emp{emp.Username, emp.Age})
+	}
+	c.JSON(http.StatusOK, gin.H{"message": emps})
 }
